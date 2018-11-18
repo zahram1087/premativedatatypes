1391bd8d5fc4c595e4f19a8b7e06fe07e33407e3
Zahra <zahram1087@gmail.com>
2018-11-18 10:09:01 -0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main (String[] args){
+//        int x = 10;
+//        System.out.println(x*x);
+        char x = 90;
+        double foo = 2.95;
+        int bar = (int)foo;
+        System.out.println(foo);
+        System.out.println(bar);
+//        System.out.println(x);
+//        premativeDataTypes();
+
+    }
+
+    public static void premativeDataTypes(){
+        System.out.println("Data Types");
+        System.out.println("byte \t -128-127");
+        System.out.println("short \t -32768-32767");
+        System.out.println("int \t -2147483648-2147483647");
+        System.out.println("long \t really long number");
+        System.out.println("float \t varies");
+        System.out.println("double \t varies ie 12.5 ");
+        System.out.println("boolean\t true/false");
+        System.out.println("char \t 0-65535");
+        System.out.println("String \t \"\"");
+    }
+}
+
+
